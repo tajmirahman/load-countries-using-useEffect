4bcd9e8613f40193442456b25406e44cbd97f8ac
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-13 17:37:59 +0600

    button update

--- load-countries/src/countries/countries.jsx
+++ load-countries/src/countries/countries.jsx
@@ -1,26 +1,31 @@
 import { useEffect } from "react";
 import { useState } from "react"
 import Country from "./country";
+import './countries.css'
 
-export default function Countries(){
+export default function Countries() {
 
-    const [countries,setCountry]=useState([]);
+    const [countries, setCountry] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res=>res.json())
-        .then(data=>setCountry(data))
+            .then(res => res.json())
+            .then(data => setCountry(data))
     }, []);
 
-    return(
-        <div>
+    return (
+        <div >
             <h3>Countries : {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-        </div> 
-           
+            <div className="country-container" >
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
+                
+
+        </div>
+
     )
 
-    
+
 }
--- load-countries/src/countries/country.jsx
+++ load-countries/src/countries/country.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './countries.css'
-export default function Country({country}){
-    const {name, flags}=country;
-    return(
+export default function Country({ country }) {
+    const { name, flags } = country;
+
+        const  [visited, setVisited]  = useState(false)
+
+    const handleVisit = () => {
+        setVisited(!visited)
+        // alert('hi')
+    }
+
+    return (
         <div className='country'>
             <h3>Name : {name?.common}</h3>
-          <img src={flags.png} alt="" />
-          
-            
+            <img className='country-flag' src={flags.png} alt="" />
+
+            <button onClick={handleVisit}>{visited ? 'Visited':'Going'}</button>
+            {visited ? 'i m visited' : 'i want to go'}
         </div>
     )
 }
